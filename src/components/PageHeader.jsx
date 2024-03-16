69efcd31bc3adf4c64ad21acaefef3e6a687f206
KurtJQ <irockstar12230@gmail.com>
2024-03-17 02:56:48 +0800

    PageHeader added

---
+++ src/components/PageHeader.jsx
@@ -0,0 +1,23 @@
+function PageHeader() {
+  const currentDate = new Date().toDateString();
+
+  return (
+    <header className="container">
+      <div className="page-header">
+        <div className="userInfo">
+          <img
+            src="src/assets/user-circle-regular-36.png"
+            alt="User Profile Picture"
+          />
+          <div className="user-name">Kurt Justine Que</div>
+        </div>
+        <div className="date">
+          <div>{currentDate}</div>
+          <img src="src/assets/calendar-solid-36.png" alt="calendar icon" />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default PageHeader;
