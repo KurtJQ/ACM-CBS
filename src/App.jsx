9ad4c00de535323d889797a06a918fd234415117
FurioKashimazu <kashimazu@mail.com>
2024-03-17 05:48:25 +0800

    header and dashboard combined

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,28 @@ import PageHeader from "./components/PageHeader";
 
 function App() {
   return (
-    <>
+    <div>
+      <header className="header">
       <PageHeader />
-    </>
+      </header>
+      
+      <div className='dashboard'>
+        <h2>DASHBOARD</h2>
+        <div className='buttons'>
+        <div class="button-container">
+          <button class="button" onclick="goToStudentRecords()">Student Records</button>
+          <button class="button" onclick="goToStudentAccounts()">Student Accounts</button>
+          <button class="button" onclick="goToSuperAdminPanel()">Super Admin Panel</button>
+        </div>
+
+        <footer>
+        <p>Log Out Account</p>
+        
+        </footer>
+        
+        </div>
+      </div>
+    </div>
   );
 }
 
