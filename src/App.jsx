60ba20ca8dd1601607eecedb46c526e7d32b7e2e
KurtJQ <irockstar12230@gmail.com>
2024-03-18 13:30:55 +0800

    studentrecords wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import PageHeader from "./components/PageHeader";
+import StudentRecords from "./components/pages/studentrecords";
 
 function App() {
   return (
     <>
       <PageHeader />
+      <StudentRecords />
     </>
   );
 }
---
+++ src/components/pages/studentrecords.jsx
@@ -0,0 +1,21 @@
+function StudentRecords() {
+  return (
+    <>
+      <div className="container">
+        <div className="header">
+          <h2>S T U D E N T R E C O R D S</h2>
+        </div>
+        <div className="main-content">
+          <div className="search-bar">search bar</div>
+          <div className="statistics">
+            <div>Number of registered Student's:</div>
+            <div>0</div>
+          </div>
+          <div className="list">list</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default StudentRecords;
