747fbfd1b171795cb5d19d73dc6a07f1e7692aa1
KurtJQ <irockstar12230@gmail.com>
2024-03-21 05:03:07 +0800

    added functionality to studentrecords (still wip)

--- src/components/pages/studentrecords.jsx
+++ src/components/pages/studentrecords.jsx
@@ -1,20 +1,53 @@
+import { render } from "react-dom";
+import "./studentrecords.css";
+import StudentList from "../StudentRecordsList";
+import list from "../SAMPLE_DATA.json";
+
+function StudentRecordsHeader() {
+  return (
+    <div className="header">
+      <h2>S T U D E N T R E C O R D S</h2>
+    </div>
+  );
+}
+
+function SearchBar() {
+  return <div className="search-bar">search bar</div>;
+}
+
 function StudentRecords() {
   return (
-    <>
-      <div className="container">
-        <div className="header">
-          <h2>S T U D E N T R E C O R D S</h2>
-        </div>
-        <div className="main-content">
-          <div className="search-bar">search bar</div>
+    <div className="container">
+      <StudentRecordsHeader />
+      <div className="main-content">
+        <div className="left-side">
+          <SearchBar />
           <div className="statistics">
             <div>Number of registered Student's:</div>
-            <div>0</div>
+            <div>{list.length}</div>
           </div>
-          <div className="list">list</div>
+        </div>
+        <div className="list">
+          {list.map((student) => (
+            <StudentList
+              key={student.student_id}
+              studentid={student.student_id}
+              firstname={student.first_name}
+              lastname={student.last_name}
+              middleinitial={student.middle_name}
+              firstperiodic={student.exams.firstperiodic}
+              prelim={student.exams.prelim}
+              secondperiodic={student.exams.secondperiodic}
+              midterm={student.exams.midterm}
+              thirdperiodic={student.exams.thirdperiodic}
+              prefinals={student.exams.prefinals}
+              fourthperiodic={student.exams.fourthperiodic}
+              finals={student.exams.finals}
+            />
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
