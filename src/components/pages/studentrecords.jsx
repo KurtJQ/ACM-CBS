c4610e654abc03d94d89c6ce46395e420c5df4db
KurtJQ <irockstar12230@gmail.com>
2024-03-21 06:26:27 +0800

    searchbar WIP

--- src/components/pages/studentrecords.jsx
+++ src/components/pages/studentrecords.jsx
@@ -2,23 +2,31 @@ import { render } from "react-dom";
 import "./studentrecords.css";
 import StudentList from "../StudentRecordsList";
 import list from "../SAMPLE_DATA.json";
-
-function StudentRecordsHeader() {
-  return (
-    <div className="header">
-      <h2>S T U D E N T R E C O R D S</h2>
-    </div>
-  );
-}
+import { useState } from "react";
 
 function SearchBar() {
-  return <div className="search-bar">search bar</div>;
+  const [query, setQuery] = useState();
+  return (
+    <div className="searchbar">
+      <label htmlFor="query" hidden>
+        Search
+      </label>
+      <input
+        type="text"
+        name="query"
+        placeholder="Search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </div>
+  );
 }
 
 function StudentRecords() {
   return (
     <div className="container">
-      <StudentRecordsHeader />
+      <div className="header">
+        <h2>S T U D E N T R E C O R D S</h2>
+      </div>
       <div className="main-content">
         <div className="left-side">
           <SearchBar />
