ac25f97ea71457a2657ea1f5dafa8e9fbd9f3e12
FurioKashimazu <kashimazu@mail.com>
2024-03-21 09:15:31 +0800

    t

--- src/components/pages/StudentAccounts.jsx
+++ src/components/pages/StudentAccounts.jsx
@@ -19,7 +19,6 @@ function SearchBar() {
     </div>
   );
 }
-
 function StudentAccountsHeader() {
   return (
     <>
@@ -41,9 +40,15 @@ function StudentAccounts() {
         <div className="search-bar">
           <SearchBar />
         </div>
+        <div className="add-button">
+          <h4>Add Student</h4>
+        </div>
+        <div className="head-column">
+          Last Name  First Name  M.I.  Student ID  Password  Email Address Contact Number 
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default StudentAccounts;
