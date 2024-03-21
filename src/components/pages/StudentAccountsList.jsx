79b588088e0bd7e65632d4bc3cd0c1d297de52d4
FurioKashimazu <kashimazu@mail.com>
2024-03-21 10:37:50 +0800

    student accounts

---
+++ src/components/pages/StudentAccountsList.jsx
@@ -0,0 +1,21 @@
+import "./StudentAccountsList.css";
+
+function StudentAccountsList(props) {
+  return (
+    <div className="studentaccount-items" id={props.studentid}>
+      <div className="last-name">{props.lastname}</div>
+      <div className="first-name">{props.firstname}</div>
+      <div className="middle-name">{props.middlename}</div>
+      <div className="cashier-id">{props.studentid}</div>
+      <div className="email">{props.email}</div>
+      <div className="password">{props.password}</div>
+      <div className="contact#">{props.contactnum}</div>
+      <div className="buttons">
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default StudentAccountsList;
