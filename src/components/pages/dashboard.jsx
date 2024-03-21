b7a9b1d326255474f01f1a3aee2eef34654091be
KurtJQ <irockstar12230@gmail.com>
2024-03-21 09:10:08 +0800

    Superadmin panel added

--- src/components/pages/dashboard.jsx
+++ src/components/pages/dashboard.jsx
@@ -23,24 +23,13 @@ function Dashboard() {
         <div className="buttons">
           <div class="button-container">
             <Link to="/studentrecords">
-              <button class="button" onclick="goToStudentRecords()">
-                Student Records
-              </button>
+              <button class="button">Student Records</button>
             </Link>
-            <button class="button" onclick="goToStudentAccounts()">
-              Student Accounts
-            </button>
-            <button class="button" onclick="goToSuperAdminPanel()">
-              Super Admin Panel
-            </button>
+            <button class="button">Student Accounts</button>
+            <Link to="/superadminpanel">
+              <button class="button">Super Admin Panel</button>
+            </Link>
           </div>
-
-          <footer>
-            <p>
-              {" "}
-              Log Out Account<img className="logout-icon" src={logout}></img>
-            </p>
-          </footer>
         </div>
       </div>
     </div>
--- src/components/pages/superadminpanel.jsx
+++ src/components/pages/superadminpanel.jsx
@@ -1,5 +1,52 @@
 import "./superadminpanel.css";
+import { Link } from "react-router-dom";
+import SuperAdminList from "../SuperAdminList";
+import data from "../SAMPLE_DATA_ADMIN.json";
 
-function SuperAdminPanel() {}
+function SuperAdminHeader() {
+  return (
+    <>
+      <div className="header">
+        <Link to="/">
+          <img src="src\assets\arrow-back-regular-48.png" alt="back button" />
+        </Link>
+        <h2>S U P E R A D M I N P A N E L</h2>
+      </div>
+    </>
+  );
+}
+
+function SuperAdminPanel() {
+  return (
+    <div className="superadminpanel-container">
+      <SuperAdminHeader />
+      <div className="superadmin-main-content">
+        <div className="superadmin-header">
+          <label htmlFor="query" hidden>
+            Search
+          </label>
+          <input type="text" name="query" placeholder="Search..." />
+          <button>Add New Account</button>
+        </div>
+        <div className="adminlist">
+          {data.map((admins) => {
+            console.log(admins);
+            return (
+              <SuperAdminList
+                firstname={admins.first_name}
+                lastname={admins.last_name}
+                middleinitial={admins.middle_name}
+                cashierid={admins.cashierID}
+                email={admins.email}
+                password={admins.password}
+                contactnum={admins.contactnum}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default SuperAdminPanel;
