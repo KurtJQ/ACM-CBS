39cb63b7995a5f2724066ce836c194deb457027c
KurtJQ <irockstar12230@gmail.com>
2024-03-26 04:03:29 +0800

    moved router from app.jsx to main.jsx (again)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,58 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 import "./index.css";
-import App from "./App";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from "./components/pages/dashboard";
+import StudentRecords from "./components/pages/studentrecords.jsx";
+import StudentAccounts from "./components/pages/StudentAccounts.jsx";
+import SuperAdminPanel from "./components/pages/superadminpanel.jsx";
+import PageHeader from "./components/PageHeader.jsx";
+import PageFooter from "./components/PageFooter.jsx";
+import NewTransaction from "./components/pages/NewTransaction.jsx";
+import LoginSystem from "./components/pages/Login.jsx";
+import NewAccount from "./components/pages/newaccount.jsx";
+import NewAdmin from "./components/pages/newadmin.jsx";
+import Root from "./components/pages/root.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginSystem />,
+  },
+  {
+    path: "dashboard",
+    element: <Dashboard />,
+  },
+
+  {
+    path: "studentrecords",
+    element: <StudentRecords />,
+  },
+  {
+    path: "studentrecords/:studentID/newtransaction",
+    element: <NewTransaction />,
+  },
+  {
+    path: "studentaccounts",
+    element: <StudentAccounts />,
+  },
+  {
+    path: "studentaccounts/new-student",
+    element: <NewAccount />,
+  },
+  {
+    path: "superadminpanel",
+    element: <SuperAdminPanel />,
+  },
+  {
+    path: "newadmin",
+    element: <NewAdmin />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
