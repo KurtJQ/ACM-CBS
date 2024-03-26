353209777361e7c32913453949cec53fa6e4ec69
KurtJQ <irockstar12230@gmail.com>
2024-03-26 12:16:22 +0800

    added logout functionality to logout button

--- src/components/PageFooter.jsx
+++ src/components/PageFooter.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import "./pagefooter.css";
 
 function PageFooter() {
   return (
     <footer>
       <p>
-        Log Out Account<img className="images\logout-icon.png"></img>
+        <Link to="login">
+          <img src="images\logout-icon.png"></img>
+        </Link>
       </p>
     </footer>
   );
