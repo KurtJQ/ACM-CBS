6fe3d16bad61b07e4ac4f9ce8a40baa4aaf1dd49
KurtJQ <irockstar12230@gmail.com>
2024-05-13 15:50:22 +0800

    header now uses the name of the person logged in

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,6 +1,8 @@
 import "./pageheader.css";
+import { useAuthContext } from "../hooks/useAuthContext.js";
 
 function PageHeader() {
+  const { user } = useAuthContext();
   const currentDate = new Date().toDateString();
 
   return (
@@ -11,7 +13,13 @@ function PageHeader() {
             src="images\user-circle-regular-36.png"
             alt="User Profile Picture"
           />
-          <div className="user-name">Jacqueline Rose Corre</div>
+          <div className="user-name">
+            {user.userID.firstname +
+              " " +
+              user.userID.middleinitial +
+              ". " +
+              user.userID.lastname}
+          </div>
         </div>
         <div className="date">
           <div>{currentDate}</div>
